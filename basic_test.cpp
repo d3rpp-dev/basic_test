974f8e74142d7801de0b7405a5b8f280788d0c28
d3rpp <hudson.curren@icloud.com>
2023-03-02 08:44:11 +1300

    gaming & pizza too

--- basic_test.cpp
+++ basic_test.cpp
@@ -36,19 +36,35 @@ namespace take {
 }
 
 int main() {
-	std::string s_a = take::string("fname"), s_b = take::string("lname");
+	std::string 
+		s_a = take::string("fname"), 
+		s_b = take::string("lname");
+
 	std::cout << "Hello, " << s_a << " " << s_b << $nl;
 
-	int i_a = take::number("a"), i_x = take::number("x"), i_b = take::number("b");
+	int 
+		i_a = take::number("a"), 
+		i_x = take::number("x"), 
+		i_b = take::number("b");
+
 	std::cout << "ax + b = " << (i_a * i_x) + i_b << $nl;
 
-	float f_radius = take::decimal("radius");
+	float 
+		f_radius = take::decimal("radius");
+
 	std::cout << "pi * r^2 = " << PI * powf(f_radius, 2) << $nl;
 
-	float f_principle = take::decimal("Principle"), f_rate = take::decimal("Interest Rate"), f_t = take::decimal("Time (years)");
+	float 
+		f_principle = take::decimal("Principle"), 
+		f_rate = take::decimal("Interest Rate"), 
+		f_t = take::decimal("Time (years)");
+
 	std::cout << "P(1+rt) = " << f_principle * (1 + (f_rate * f_t)) << $nl;
 
-	float f_x = take::decimal("x"), f_y = take::decimal("y");
+	float 
+		f_x = take::decimal("x"), 
+		f_y = take::decimal("y");
+
 	std::cout << "x^2 + y^2 = " << powf(f_x, 2) + powf(f_y, 2) << $nl;
 
 	return 0;
