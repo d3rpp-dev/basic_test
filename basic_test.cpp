587a754fa478d57cd9589939ec55e260ab15bc25
d3rpp <hudson.curren@icloud.com>
2023-03-06 11:33:04 +1300

    GARBAGE

--- basic_test.cpp
+++ basic_test.cpp
@@ -53,13 +53,30 @@ namespace ops {
 	T div(T lhs, T rhs) {
 		return lhs / rhs;
 	}
+
+	float q_rsqrt(float number)
+	{
+		long i;
+		float x2, y;
+		const float threehalfs = 1.5F;
+
+		x2 = number * 0.5F;
+		y = number;
+		i = *(long*)&y;							// evil floating point bit level hacking
+		i = 0x5f3759df - (i >> 1);              // what the f*ck? 
+		y = *(float*)&i;
+		y = y * (threehalfs - (x2 * y * y));    // 1st iteration
+		//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+		return y;
+	}
 }
 
 namespace fns {
 	void print_nums() {
 		int
-			f_num{ 17 },
-			s_num{ 3 };
+			f_num = 17,
+			s_num = 3;
 
 		std::cout << "First Number: " << f_num << std::endl << "Second Number: " << s_num << NL;
 	}
@@ -70,6 +87,15 @@ namespace fns {
 		int b = a << 1;
 
 		std::cout << "A: " << a << std::endl << "B: " << b << NL;
+	}
+
+	void random_garbage() {
+		for (int _i = 0; _i < 100; _i++) {
+			int a = std::rand();
+
+			std::cout << "random garbage: " << a << std::endl;
+		}
+		std::cout << std::endl;
 	}
 
 	void print_name() {
@@ -121,7 +147,7 @@ namespace fns {
 		ops::mul(a, b);
 		ops::div(a, b);
 
-		float c = 3.12, d = 2.8;
+		float c = (float)3.12, d = (float)2.8;
 
 		ops::add(c, d);
 		ops::sub(c, d);
@@ -140,12 +166,13 @@ namespace fns {
 int main() {
 	fns::test_numbers();
 	fns::bitshift_neg();
-	fns::print_nums();
-	fns::print_name();
-	fns::quadratic();
-	fns::area();
-	fns::compounding_interest();
-	fns::graph_a_circle();
+	fns::random_garbage();
+	// fns::print_nums();
+	// fns::print_name();
+	// fns::quadratic();
+	// fns::area();
+	// fns::compounding_interest();
+	// fns::graph_a_circle();
 
 	return 0;
 }
