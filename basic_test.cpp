b9f924aa372d0fa7f8c30af91387e43220a0e288
d3rpp <hudson.curren@icloud.com>
2023-03-06 09:48:35 +1300

    Added another function put functions in `ops` namespace to make it look cleaner added common header file for importing deps from std

--- basic_test.cpp
+++ basic_test.cpp
@@ -1,15 +1,12 @@
-﻿#include <iostream>
-#include <string>
-#include <cmath>
+﻿#include "common.h"
+#define NL std::endl << std::endl
 
-#define $nl std::endl << std::endl
-
-const float PI = 3.141592;
-const std::string PROMPT = " >> ";
+constexpr auto PI = 3.14159;
+constexpr auto PROMPT = " = ";
 
 namespace take {
-	std::string string(std::string q) {
-		std::cout << q << std::endl << PROMPT;
+	std::string string(std::string query) {
+		std::cout << query << PROMPT;
 		std::string a;
 
 		std::getline(std::cin, a);
@@ -17,8 +14,8 @@ namespace take {
 		return a;
 	}
 
-	int number(std::string q) {
-		std::cout << q << std::endl << PROMPT;
+	int number(std::string query) {
+		std::cout << query << PROMPT;
 		int i;
 
 		std::cin >> i;
@@ -26,8 +23,8 @@ namespace take {
 		return i;
 	}
 
-	float decimal(std::string q) {
-		std::cout << q << std::endl << PROMPT;
+	float decimal(std::string query) {
+		std::cout << query << PROMPT;
 		float f;
 
 		std::cin >> f;
@@ -36,37 +33,64 @@ namespace take {
 	}
 }
 
+namespace ops {
+	void print_nums() {
+		int
+			f_num{ 17 },
+			s_num{ 3 };
+
+		std::cout << "First Number: " << f_num << std::endl << "Second Number: " << s_num << NL << "\x0a\x0d";
+	}
+
+	void print_name() {
+		std::string
+			s_a = take::string("fname"),
+			s_b = take::string("lname");
+
+		std::cout << "Hello, " << s_a << " " << s_b << NL;
+	}
+
+	void quadratic() {
+		int
+			i_a = take::number("a"),
+			i_x = take::number("x"),
+			i_b = take::number("b");
+
+		std::cout << "ax + b = " << (i_a * i_x) + i_b << NL;
+	}
+
+	void area() {
+		float
+			f_radius = take::decimal("radius");
+
+		std::cout << "pi * r^2 = " << PI * powf(f_radius, 2) << NL;
+	}
+
+	void compounding_interest() {
+		float
+			f_principle = take::decimal("Principle"),
+			f_rate = take::decimal("Interest Rate"),
+			f_t = take::decimal("Time (years)");
+
+		std::cout << "P(1+rt) = " << f_principle * (1 + (f_rate * f_t)) << NL;
+	}
+
+	void graph_a_circle() {
+		float
+			f_x = take::decimal("x"),
+			f_y = take::decimal("y");
+
+		std::cout << "x^2 + y^2 = " << powf(f_x, 2) + powf(f_y, 2) << NL;
+	}
+}
+
 int main() {
-	std::string 
-		s_a = take::string("fname"), 
-		s_b = take::string("lname");
-
-	std::cout << "Hello, " << s_a << " " << s_b << $nl;
-
-	int 
-		i_a = take::number("a"), 
-		i_x = take::number("x"), 
-		i_b = take::number("b");
-
-	std::cout << "ax + b = " << (i_a * i_x) + i_b << $nl;
-
-	float 
-		f_radius = take::decimal("radius");
-
-	std::cout << "pi * r^2 = " << PI * powf(f_radius, 2) << $nl;
-
-	float 
-		f_principle = take::decimal("Principle"), 
-		f_rate = take::decimal("Interest Rate"), 
-		f_t = take::decimal("Time (years)");
-
-	std::cout << "P(1+rt) = " << f_principle * (1 + (f_rate * f_t)) << $nl;
-
-	float 
-		f_x = take::decimal("x"), 
-		f_y = take::decimal("y");
-
-	std::cout << "x^2 + y^2 = " << powf(f_x, 2) + powf(f_y, 2) << $nl;
+	ops::print_nums();
+	ops::print_name();
+	ops::quadratic();
+	ops::area();
+	ops::compounding_interest();
+	ops::graph_a_circle();
 
 	return 0;
 }
