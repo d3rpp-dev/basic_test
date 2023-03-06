aec0dbd9e78fb074dac312363f2abc2f9e6d9427
d3rpp <hudson.curren@icloud.com>
2023-03-07 11:41:08 +1300

    added a little CLI calculator, it's shit lmao

--- basic_test.cpp
+++ basic_test.cpp
@@ -1,52 +1,38 @@
 ﻿#include "common.h"
 #define NL std::endl << std::endl
 
-#define _CRT_SECURE_NO_WARNINGS
-#pragma warning(disable : 4996)
-
-#define u8 char
-#define u16 short unsigned int
-#define u32 unsigned int
-#define u64 unsigned long
-#define u128 unsigned long long
-
-#define i8 signed char
-#define i16 short signed int
-#define i32 signed int
-#define i64 signed long
-#define i128 signed long long
-
-#define f32 float
-#define f64 double
-
-#define str std::string
-
-constexpr auto PI = (float)3.14159;
-constexpr auto PROMPT = " = ";
+constexpr auto PI = (float)3.1415926536;
+constexpr auto PROMPT = "\n > ";
 
 namespace take {
-	str stri(str query) {
-		std::cout << query << PROMPT;
+	str text(str query) {
+		std::cout << GREEN << query << RESET << PROMPT;
 		str a;
 
+		std::cin.clear();
+		fflush(stdin);
 		std::getline(std::cin, a);
 
 		return a;
 	}
 
-	int number(str query) {
-		std::cout << query << PROMPT;
-		int i;
+	str word(str query) {
+		std::cout << RED << query << RESET << PROMPT;
+		str a;
 
-		std::cin >> i;
+		std::cin.clear();
+		fflush(stdin);
+		std::cin >> a;
 
-		return i;
+		return a;
 	}
 
-	float decimal(str query) {
-		std::cout << query << PROMPT;
-		float f;
+	template<typename T>
+	T number(str query) {
+		std::cout << BLUE << query << RESET << PROMPT;
+		T f{};
 
+		fflush(stdin);
 		std::cin >> f;
 
 		return f;
@@ -73,24 +59,6 @@ namespace ops {
 	T div(T lhs, T rhs) {
 		return lhs / rhs;
 	}
-
-	// just for the funny, I've copied the infamous q_rsqrt algorithm
-	float q_rsqrt(float number)
-	{
-		long i;
-		float x2, y;
-		const float threehalfs = 1.5F;
-
-		x2 = number * 0.5F;
-		y = number;
-		i = *(long*)&y;							// evil floating point bit level hacking
-		i = 0x5f3759df - (i >> 1);              // what the f*ck? 
-		y = *(float*)&i;
-		y = y * (threehalfs - (x2 * y * y));    // 1st iteration
-		//	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
-
-		return y;
-	}
 }
 
 namespace fns {
@@ -99,7 +67,7 @@ namespace fns {
 			f_num = 17,
 			s_num = 3;
 
-		std::cout << "First Number: " << f_num << std::endl << "Second Number: " << s_num << NL;
+		std::cout << "First Number" << f_num << std::endl << "Second Number" << s_num << NL;
 	}
 
 	void bitshift_neg() {
@@ -122,41 +90,41 @@ namespace fns {
 
 	void print_name() {
 		str
-			s_a = take::stri("fname"),
-			s_b = take::stri("lname");
+			s_a = take::text("fname"),
+			s_b = take::text("lname");
 
 		std::cout << "Hello, " << s_a << " " << s_b << NL;
 	}
 
 	void quadratic() {
 		i32
-			i_a = take::number("a"),
-			i_x = take::number("x"),
-			i_b = take::number("b");
+			i_a = take::number<i32>("a"),
+			i_x = take::number<i32>("x"),
+			i_b = take::number<i32>("b");
 
 		std::cout << "ax + b = " <<  ops::add(ops::mul(i_a, i_x), i_b) << NL;
 	}
 
 	void area() {
 		f32
-			f_radius = take::decimal("radius");
+			f_radius = take::number<f32>("radius");
 
 		std::cout << "pi * r^2 = " << ops::mul(PI, powf(f_radius, 2)) << NL;
 	}
 
 	void compounding_interest() {
 		f32
-			f_principle = take::decimal("Principle"),
-			f_rate = take::decimal("Interest Rate"),
-			f_t = take::decimal("Time (years)");
+			f_principle = take::number<f32>("Principle"),
+			f_rate = take::number<f32>("Interest Rate"),
+			f_t = take::number<f32>("Time (years)");
 
 		std::cout << "P(1+rt) = " << ops::mul(f_principle, ops::add(1.0f, ops::mul(f_rate, f_t))) << NL;
 	}
 
 	void graph_a_circle() {
 		f32
-			f_x = take::decimal("x"),
-			f_y = take::decimal("y");
+			f_x = take::number<f32>("x"),
+			f_y = take::number<f32>("y");
 
 		std::cout << "x^2 + y^2 = " << ops::add(powf(f_x, 2), powf(f_y, 2)) << NL;
 	}
@@ -164,37 +132,158 @@ namespace fns {
 	void test_numbers() {
 		i32 a = 1, b = 2;
 
-		ops::add(a, b);
-		ops::sub(a, b);
-		ops::mul(a, b);
-		ops::div(a, b);
+		i32 i_add = ops::add(a, b);
+		i32 i_sub = ops::sub(a, b);
+		i32 i_mul = ops::mul(a, b);
+		i32 i_div = ops::div(a, b);
 
 		f32 c = (float)3.12, d = (float)2.8;
 
-		ops::add(c, d);
-		ops::sub(c, d);
-		ops::mul(c, d);
-		ops::div(c, d);
+		f32 f_add = ops::add(c, d);
+		f32 f_sub = ops::sub(c, d);
+		f32 f_mul = ops::mul(c, d);
+		f32 f_div = ops::div(c, d);
+
 
 		f64 e = 1.12, f = 2.342;
 
-		ops::add(e, f);
-		ops::sub(e, f);
-		ops::mul(e, f);
-		ops::div(e, f);
+		f64 d_add = ops::add(e, f);
+		f64 d_sub = ops::sub(e, f);
+		f64 d_mul = ops::mul(e, f);
+		f64 d_div = ops::div(e, f);
+
+		f128 g = 1.12, h = 2.342;
+
+		f128 l_add = ops::add(g, h);
+		f128 l_sub = ops::sub(g, h);
+		f128 l_mul = ops::mul(g, h);
+		f128 l_div = ops::div(g, h);
+
+		str s;
+		std::cout << " >> ";
+		std::getline(std::cin, s);
+
+		std::cout << "not runtime erros from templates" << NL;
+	}
+
+	void size_of_vars() {
+		std::cout
+			<< "i8:   " << sizeof(i8)   << std::endl
+			<< "i16:  " << sizeof(i16)  << std::endl
+			<< "i32:  " << sizeof(i32)  << std::endl
+			<< "i64:  " << sizeof(i64)  << std::endl
+			<< "f32:  " << sizeof(f32)  << std::endl
+			<< "f64:  " << sizeof(f64)  << std::endl
+			<< "f128: " << sizeof(f128) << std::endl;
+	}
+
+	void floating_point() {
+		f32  flt = 1.2132321323124324321432456523653241356432156234f;
+		f64  dbl = 1.2132321323124324321432456523653241356432156234;
+		f128 ldb = 1.2132321323124324321432456523653241356432156234L;
+
+		f64 dsa = 1.342435325536245635214e16;
+		f64 bru = 3.498e-12;
+
+		f64 a = 5.6, b = 0.0;
+
+		f64 c = a / b;
+
+		std::cout << std::setprecision(20);
+		std::cout << "bse: 1.2132321323124324321432456523653241356432156234\n";
+		std::cout << "flt: " << flt << "\n";
+		std::cout << "dbl: " << dbl << "\n";
+		std::cout << "ldb: " << ldb << "\n";
+		std::cout << "dsa: " << dsa << "\n";
+		std::cout << "bru: " << bru << "\n";
+		std::cout << "c  : " << c << "\n";
+	}
+
+	void conditional_programming_is_incredibly_easy() {
+		bool a = true, b = false;
+
+		if (a) {
+			std::cout << "a == true" << NL;
+		}
+		else {
+			std::cout << "a == false" << NL;
+		}
+	}
+}
+
+namespace more_fns {
+	void multiple_choice() {
+		bool should_exit = false;
+
+		do {
+			f64 input_one = 0.0, input_two = 0.0, output = 0.0;
+
+			char op = '\x00';
+			bool input_valid = false;
+
+			do {
+				str operand = take::word("Operand (or e to exit) [+-*/e]");
+
+				trim(operand);
+
+				if (operand.length() < 1) { 
+					std::cout << std::endl;
+					continue; 
+				}
+
+				if (operand[0] == '+' || operand[0] == '-' || operand[0] == '*' || operand[0] == '/' || operand[0] == 'e') {
+					op = operand[0];
+					input_valid = true;
+				}
+			// operand check
+			} while (!input_valid);
+
+			if (op == 'e') {
+				// user has chosen to exit
+				should_exit = true;
+				continue;
+			}
+
+			input_one = take::number<f64>("Left Hand Side");
+			input_two = take::number<f64>("Right Hand Side");
+
+			// we use numbers because why tf not
+
+			std::cout << std::endl;
+
+			if (op == 43) {
+				std::cout << GREEN << input_one << RESET << " + " << GREEN << input_two << RESET << " = " << CYAN << ops::add(input_one, input_two) << RESET << NL;
+			} 
+			else if (op == 42) {
+				std::cout << GREEN << input_one << RESET << " * " << GREEN << input_two << RESET << " * " << CYAN << ops::mul(input_one, input_two) << RESET << NL;
+			}
+			else if (op == 45) {
+				std::cout << GREEN << input_one << RESET << " - " << GREEN << input_two << RESET << " = " << CYAN << ops::sub(input_one, input_two) << RESET << NL;
+			}
+			else if (op == 47) {
+				std::cout << GREEN << input_one << RESET << " / " << GREEN << input_two << RESET << " = " << CYAN << ops::div(input_one, input_two) << RESET << NL;
+			}
+
+		// main loop
+		} while (!should_exit);
 	}
 }
 
 int main() {
-	fns::test_numbers();
-	fns::bitshift_neg();
-	fns::random_garbage();
+	// fns::floating_point();
+	// fns::conditional_programming_is_incredibly_easy();
+	// fns::bitshift_neg();
+	// fns::size_of_vars();
+	// fns::test_numbers();
+	// fns::random_garbage();
 	// fns::print_nums();
 	// fns::print_name();
 	// fns::quadratic();
 	// fns::area();
 	// fns::compounding_interest();
 	// fns::graph_a_circle();
+
+	more_fns::multiple_choice();
 
 	return 0;
 }
--- common.h
+++ common.h
@@ -2,3 +2,55 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <iomanip>
+
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 4996)
+
+#define u8 char
+#define u16 unsigned short
+#define u32 unsigned int
+#define u64 unsigned long long int
+
+#define i8 signed char
+#define i16 signed short
+#define i32 signed int
+#define i64 signed long long int
+
+#define f32 float
+#define f64 double
+#define f128 long double
+
+#define str std::string
+
+
+/* 
+ * ******************************
+ *   Borrowed from the internet
+ * ******************************
+ */
+// trim from start (in place)
+static inline void ltrim(std::string& s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+        return !std::isspace(ch);
+        }));
+}
+
+// trim from end (in place)
+static inline void rtrim(std::string& s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+        }).base(), s.end());
+}
+
+// trim from both ends (in place)
+static inline void trim(std::string& s) {
+    ltrim(s);
+    rtrim(s);
+}
+
+constexpr auto RESET = "\x1b[39;40m";
+constexpr auto RED =   "\x1b[91;40m";
+constexpr auto GREEN = "\x1b[92;40m";
+constexpr auto BLUE =  "\x1b[94;40m";
+constexpr auto CYAN =  "\x1b[96;40m";
